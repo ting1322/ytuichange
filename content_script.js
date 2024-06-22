d84a1fcf9944c0af29f43316fc00a522455fb975
ting <bullian99@gmail.com>
2024-06-22 14:59:46 +0800

    init

---
+++ content_script.js
@@ -0,0 +1,13 @@
+// Put all the javascript code here, that you want to execute after page load.
+
+function m() {
+  var actions = document.getElementById("actions-inner");
+
+  var player = document.getElementById("player");
+
+  var below = document.getElementById("below");
+
+  below.parentElement.insertBefore(actions, below);
+}
+
+setTimeout(m, 5000);
