f3ea7f7d77f04338611d3998fd108a96276e7b07
ting <bullian99@gmail.com>
2024-07-02 21:28:51 +0800

    fix: chat room pannel hidden
    不知道為啥聊天室有時候沒顯示，總之叫他顯示。

--- content_script.js
+++ content_script.js
@@ -1,40 +1,48 @@
 // Put all the javascript code here, that you want to execute after page load.
 
 function m() {
-  var actions = document.getElementById("actions-inner");
-  var player = document.getElementById("player");
-  var below = document.getElementById("below");
-  var info = document.getElementById("info-container");
-  var desc = document.querySelector("div#description");
+    var actions = document.getElementById("actions-inner");
+    var player = document.getElementById("player");
+    var below = document.getElementById("below");
+    var info = document.getElementById("info-container");
+    var desc = document.querySelector("div#description");
+    //var chatContainer = document.querySelector("div#chat-container");
+    var chatroom = document.querySelector("ytd-live-chat-frame#chat");
 
-  if (actions == null ||
-      player == null ||
-      below == null ||
-      info == null ||
-      desc == null) {
+    chatroom.style.visibility = "visible";
 
-    setTimeout(m, 1000);
-    return;
-  }
+    if (actions == null ||
+        player == null ||
+        below == null ||
+        info == null ||
+        desc == null ||
+        chatroom == null) {
 
-  below.parentElement.insertBefore(actions, below);
-  player.style.marginBottom = "10px";
+        setTimeout(m, 3000);
+        return;
+    }
 
-  var infodiv = document.createElement('div');
-  infodiv.style.color="white";
-  below.parentElement.insertBefore(infodiv, below);
-  infodiv.appendChild(desc);
+    actions.remove();
+    below.parentElement.insertBefore(actions, below);
+    player.style.marginBottom = "10px";
+
+    desc.remove();
+    var infodiv = document.createElement('div');
+    infodiv.style.color = "white";
+    below.parentElement.insertBefore(infodiv, below);
+    infodiv.appendChild(desc);
 }
 
-(function(history){
-    var pushState = history.pushState;
-    history.pushState = function(state) {
-      // YOUR CUSTOM HOOK / FUNCTION
-      console.log('I am called from pushStateHook');
-      if (/\/(watch|live)/.test(document.URL))
-        setTimeout(m, 3000);
-      return pushState.apply(history, arguments);
-    };
-})(window.history);
+//(function(history) {
+//    var pushState = history.pushState;
+//    history.pushState = function(state) {
+//        // YOUR CUSTOM HOOK / FUNCTION
+//        console.log('I am called from pushStateHook');
+//        if (/\/(watch|live)/.test(document.URL)) {
+//            setTimeout(m, 8000);
+//        }
+//        return pushState.apply(history, arguments);
+//    };
+//})(window.history);
 
 setTimeout(m, 3000);
